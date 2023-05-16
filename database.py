8a24db81793f16f853ed11342787a544fbf9d0a1
abineshrr <abirajarani2002@gmail.com>
2023-05-15 23:55:06 -0700

    setup database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:abirr02@localhost/LoginDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://vxriylsw:zt12o5G7RnZzrkpij37kiWPyrl_PmPNS@rosie.db.elephantsql.com/vxriylsw'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
