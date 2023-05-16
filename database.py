da981766997062caa98c57c326c9db81c2d94204
abineshrr <abirajarani2002@gmail.com>
2023-05-15 22:09:19 -0700

    postgresql path changed

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:abirr02@localhost/LoginDatabase'
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:abirr02@localhost/studentlogin'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
