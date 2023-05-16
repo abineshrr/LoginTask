b7fb4f67306327da0b90269975736ec2b9cb8c65
abineshrr <abirajarani2002@gmail.com>
2023-05-15 21:49:22 -0700

    postgresql path changed

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from psycopg2 import ssl
 
+ssl_context = ssl.create_default_context()
 engine = create_engine(
     "postgresql://postgres:abirr02@localhost/LoginDatabase",
-    client_encoding="latin1",
-    sslmode="require",
-    pool_size=20,
-    max_overflow=50,
+    connect_args={"sslmode": "require", "ssl": ssl_context},
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
