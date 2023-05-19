9c08fe2d282400077ce1752045ba467d50d7abb2
abineshrr <abirajarani2002@gmail.com>
2023-05-19 05:28:10 -0700

    db error cleared

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgres://upfhbwlm:bMigRZzCz4ogSJpM00W29UhlE_dOFS_Q@drona.db.elephantsql.com/upfhbwlm'
+SQLALCHEMY_DATABASE_URL = 'postgresql://upfhbwlm:bMigRZzCz4ogSJpM00W29UhlE_dOFS_Q@drona.db.elephantsql.com/upfhbwlm'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
