1e4a569759059feb19d2270cf798eb06c154c1ad
abineshrr <abirajarani2002@gmail.com>
2023-05-30 22:02:25 -0700

    added cryptography

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgres://tbwaverb:4sE9-OBQuOgeyvJ4Igf2l0rMO2uNMc-2@suleiman.db.elephantsql.com/tbwaverb'
+SQLALCHEMY_DATABASE_URL = 'postgresql://tbwaverb:4sE9-OBQuOgeyvJ4Igf2l0rMO2uNMc-2@suleiman.db.elephantsql.com/tbwaverb'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
